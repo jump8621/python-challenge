9f7ec72853a18537e0d48f5601aa1c175767d4b9
Juliann Pezzullo <jump8621@gmail.com>
2020-09-18 21:55:08 -0400

    add data file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,8 +9,6 @@ def sum(numbers):
     total = total + 1
     return total
 
-#funtion to get unique values
-#def unique(all_canidates):
 
 election_data_csv = os.path.join('resources', 'election_data.csv')
 
@@ -24,69 +22,28 @@ with open(election_data_csv, 'r') as csvfile:
     can_list = []
 
     numb_votes = {}
-    #numb_votes = dict()
-    
-
-    #numb_votes['candidate'] = 'Khan'
-    #numb_votes["candidate"] = can_list
-    #numb_votes["votes"] = 1 
 
     for row in csvreader:
-        
         
         voter_ID.append(row[0])
        
         electees = row[2]
         if electees not in can_list:
             can_list.append(electees)
+
         if electees not in numb_votes:
             numb_votes[electees] = 1
         else:
             numb_votes[electees] = numb_votes[electees] + 1
-        #numb_votes[electees] = 1
-        #numb_votes[electees] = can_list + 1
-        #numb_votes = {electees}
-        #if "candidate" == 'Khan':
-            #numb_votes: "votes" + 1
-        #elif "candidate" == 'Correy':
-            #numb_votes: "votes" + 1
-        #elif "candidate" ==  'Li':
-            #numb_votes: "votes" + 1
-        #elif "candidate" == "O'Tooley":
-            #numb_votes: "votes" + 1 
-
-        
-
-
    
         
-    print(numb_votes)    
     print(len(voter_ID))
+    print(numb_votes)    
     print(can_list)
     for x in can_list:
-
+        
         print(x,numb_votes[x],(numb_votes[x]/(len(voter_ID))*100))
+        print(x,< value numb_votes[x])
         
-    #print(f'{numb_votes["candidates"]}{numb_votes["votes"]}') 
-    #print("candidates" + "votes")
-
-   # print(all_canidates)
-        #all_canidates = unique(electees)
-       # for word in unique_all_canidates:
-        #    if word not in unique_all_canidates:
-         #       unique_all_canidates.append(word)
-        #for word in unique_all_canidates:
-         #   print(word)
-        
-
-        
-
-      #  value = int(row[1])
-       # total = value + total
-        #if (savedvalue != 0):
-         #   profitloss = value - savedvalue            
-          #  pl_value.append(profitloss)
-        #savedvalue = value
-        #month_total.append(row[0])
-
-
+        #if numb_votes[x] > saved x then winner!
+    #print(winner)
